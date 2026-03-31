78f43683057e361563b6c28730419366b47dd63d
NesaraRahal <rahalnesara@gmail.com>
2026-03-31 10:45:00 +0530

    Initial Commit

---
+++ EdVenture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EdVenture
+//
+//  Created by COBSCCOMP24.2p-053 on 2026-03-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdVenture/EdVentureApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdVentureApp.swift
+//  EdVenture
+//
+//  Created by COBSCCOMP24.2p-053 on 2026-03-31.
+//
+
+import SwiftUI
+
+@main
+struct EdVentureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
